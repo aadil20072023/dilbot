d6d6d8a94b715a536d66057c89ebe230d211562d
aadil20072023 <aadilmax2023@gmail.com>
2026-01-19 11:52:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "E7wlXB5K#FCxSaUs6drPJlAyQoAE_U-YmDhlRxuvakyIuxLa65Aw",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/aadil20072023/dilbot/blob/main/images/Welcome_dil_bot.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/aadil20072023/dilbot/blob/main/images/welcome_dilbot.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DIL-BOT Is Alive Now😍*",
 BOT_OWNER: '94725023747',  // Replace with the owner's phone number
 
